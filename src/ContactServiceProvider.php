b44a00f9c928c1be8a5999c5aa3b9414334733e9
Ra-is <osumanurais@gmail.com>
2022-12-11 21:44:22 +0000

    modifeied

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -12,7 +12,10 @@ class ContactServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
         $this->publishes([
             __DIR__.'/config/contact.php' => config_path('contact.php'),
+            __DIR__.'/views' => resource_path('views/vendor/contact'),
         ]);
+
+       
     }
 
     public function register()
